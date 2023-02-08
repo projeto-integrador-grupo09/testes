d70c6144ed81ffa5b9b61c2bd95331c48fb8df4a
thiagoBiancareli <thiagob2mt@gmail.com>
2023-02-08 12:06:44 -0300

    Colearning validações de form

--- public/js/script.js
+++ public/js/script.js
@@ -6,13 +6,21 @@ window.onload = function () {
     let inputName = document.getElementById('nome');
 
     inputName.addEventListener('blur', (event) => {
-        if (inputName.value.length <=3 || inputName.value.length <= '3') {
-            alert('Preencher o campo com os critérios necessários');
-        } else if (inputName.value.length == "") {
-            alert ('Nome não informado');
+        if (inputName.value.length == 0) {
+            alert('Nome não informado');
+        } else if (inputName.value.length <=3) {
+            alert ('Preencher o campo com os critérios necessários');
         } else {
-            alert ('Proximo campo')
+            alert ('Proximo campo');
         };
     });
 
 };
+
+// let inputSenha = document.getElementById('psw');
+// let inputConfSenha = document.getElementById('psw-conf');
+
+// if (inputSenha.value != inputConfSenha){
+//    alert ('Senhas não conferem');
+// }
+
