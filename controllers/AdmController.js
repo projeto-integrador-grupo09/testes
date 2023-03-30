8f0a68a8bdb3af302e3152451d1f0ecd49f46db0
Mariana Mesquita <marianamesquita16@gmail.com>
2023-03-30 15:03:32 -0300

    CRUD clientes

--- controllers/AdmController.js
+++ controllers/AdmController.js
@@ -4,7 +4,8 @@ const fs = require('fs');
 const bcrypt = require('bcrypt');
 const admin = require('../databases/admin.json');
 const path = require('path');
-const administradores = require('../databases/administradores.json')
+const administradores = require('../databases/administradores.json');
+const clientes = require('../databases/clientes.json');
 
 const AdmController = {
     listarProdutos: (req, res) => {
@@ -149,19 +150,70 @@ const AdmController = {
         res.redirect('/adm/produtos');
 
 
+    },
+    //Clientes
+    listarClientes: (req, res) => {
+
+        // Renderizar view listar usuario, passando o produto para ela
+        res.render('lista-clientes.ejs', { cliente })
+
+    },
+
+    criarCliente: (req, res) => {
+        res.render('form-add-clientes.ejs')
+    },
+
+    gravarCliente: (req, res) => {
+        let novoIdCliente = 1
+        if (clientes.length > 0) {
+            novoIdCliente = clientes.at(-1).id + 1
+        }
+        let senhaCriptografadaCliente = bcrypt.hashSync(req.body.senha, 10)
+        let cliente = {
+            "id": novoIdCliente,
+            "nome": req.body.nome,
+            "email": req.body.email,
+            "senha": senhaCriptografadaCliente,
+        }
+        cliente.push(usuario);
+        fs.writeFileSync(path.join(__dirname, "..", "databases", "clientes.json"), JSON.stringify(cliente, null, 4))
+
+
+        res.redirect('/adm/clientes');
+
+    },
+
+    mostrarLogin: (req, res) => {
+        res.render('login.ejs');
+    },
+
+    login: (req, res) => {
+        // Capturar o email e a senha digitadas pelo usuário
+        const { email, senha } = req.body
+
+
+        // Verificar a existencia do cliente 
+        let cliente = clientes.find(cliente => cliente.email === email);
+        if (cliente === undefined) {
+            return res.send("Falha no login");
+        }
+        // Verificar a senha do cliente
+        let senhaCryptCliente = bcrypt.compareSync(senha, cliente.senha);
+        if (!senhaCrypt) {
+            return res.send("Falha na senha");
+        }
+
+
+        // Criar a session/cookie do adm  >>>>PAREI AQUI
+        //  1 - Baixar a biblioteca npm i express-session
+        //  2 - Configura o setup para lidar com sessions no app.js
+        req.session.admLogado = true;
+
+        // Redireciona-lo para /adm/produtos
+        res.redirect('/adm/produtos');
+
+
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
 
 module.exports = AdmController
