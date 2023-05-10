a96b56dc9551a2f4d2e1e8675ca921ed6452928c
Caio Lopes <caiolopeis95@gmail.com>
2023-05-10 19:49:43 -0300

    arrumando carrinho e associação de enderecos

--- databases/der/dh_sports.sql
+++ databases/der/dh_sports.sql
@@ -105,7 +105,8 @@ insert into produtos (id,nome,preco,categorias_id) values
 (7,"Camisa Japao",350,1);
 
 insert into enderecos (id,clientes_id,bairro,logradouro,numero,cidade,cep) values
-(1,1,"bela vista","rua modesto de paiva","126","belo horizonte","36301026");
+(1,1,"bela vista","rua modesto de paiva","126","belo horizonte","36301026"),
+(2,2,"jardim acacia","rua das flores","16","sao paulo","63709040");
 
 insert into pedidos (id,enderecos_id,clientes_id,forma_de_pagamento_id) values
 (1,1,1,2)
--- tests/enderecos.test.js
+++ tests/enderecos.test.js
@@ -1,8 +1,8 @@
 const { Enderecos, sequelize } = require('../databases/models');
 
 async function teste(){
-    let enderecos = await Enderecos.findAll({raw:true});
-        console.log(enderecos);
+    let enderecos = await Enderecos.findAll({include: "cliente"});
+        console.log(enderecos.map(e=>e.toJSON()));
     sequelize.close();
 }
 
